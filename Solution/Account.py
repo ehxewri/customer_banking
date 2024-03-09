a009aac68e69707029814b5ea6c8225cffff3770
ehxewri <frank.hanan@fjhit.com>
2024-03-09 16:19:54 -0600

    almost done.. just  a bit more playing

--- Solution/Account.py
+++ Solution/Account.py
@@ -1,34 +1,28 @@
-""" Create a Account class with methods"""
-
+# Create a Account class with methods
 class Account:
-    """Creating an Account class with methods"""
-    def __init__(self,account, balance, interest_rate,length_of_deposit,interest_earned):
+    # this methood initializes the instance
+    def __init__(self,account):
         self.account            = account 
-        self.balance            = balance
-        self.interest_rate      = interest_rate
-        self.length_of_deposit  = length_of_deposit
-        self.interest_earned    = interest_earned
-
-    # This method sets the balance of the account.
+        self.balance            = 0
+        self.interest_rate      = 0
+        self.length_of_deposit  = 0
+        self.interest_earned    = 0
+    # This methood sets the balance of the account.
     def get_account_info(self):
         self.balance            = float(input(f'\nPlease enter your initial {self.account} account balanace : '))
         self.interest_rate      = float(input(f'Please enter your {self.account} account interest rate : '))
         self.length_of_deposit  = int(input(f'Please enter your {self.account} account deposit duration in months : '))
-
-    # The method sets the interest gained for the account.
-    def calc_accrual(self):
+    # The methood sets the interest gained for the account.
+    def calc_accrued(self):
         """Sets the interest gained for the the account"""
         self.interest_earned    = (self.balance*(self.interest_rate/100/12)*self.length_of_deposit)
-
+    # The methood updates the balance for the account.
     def update_balance(self):
         """Sets the balance for the for the account"""
         self.balance            = self.balance + self.interest_earned
-
+    # The methood prints the interest accrued for the account.
     def print_interest_accrued(self):
         print (f'\nYour {self.account} account interest at maturity for {self.length_of_deposit} months is {self.interest_earned}')
+    # The method prints the updated balance for the account.
     def print_updated_balance(self):
         print (f'Your new {self.account} balance will be {self.balance}\n')
-
-class Accounts:
-    def __init__(self,accounts):
-        self.accounts = accounts
--- Solution/customer_banking.py
+++ Solution/customer_banking.py
@@ -1,26 +1,27 @@
 # import Accounts and Account class from Account
-from Account import Accounts, Account
+from Account import Account
 # import addional packages to support formating
 from rich.console import Console
 console = Console()
 console.clear()
+
 # Define the main function
 def main(account_list):
-    # instanciate my_account from Accounts class
-    my_accounts=Accounts(account_list)    
+    # instanciate my_account from Accounts class    
     """ for each account type
         Initialize the account
+        get account info
         calculate the intereste accrued
         calculat the new balance
         print interest accrued and new balance
     """ 
-    for account in my_accounts.accounts:
+    for account in account_list:
         # instanciate account from account class 
-        my_account=Account(account,0,0,0,0)
+        my_account=Account(account)
         # Prompt the user to set the savings balance, interest rate, and months for the savings account.
         my_account.get_account_info()
         # Calculate Interest accrued 
-        my_account.calc_accrual()
+        my_account.calc_accrued()
         # update the account balance
         my_account.update_balance()
         # print (f'\nYour {account} account interest at maturity for {length_of_deposit} months is {interest_earned}')
@@ -29,6 +30,7 @@ def main(account_list):
         my_account.print_updated_balance()
 
 if __name__ == "__main__":
-    account_list=['Savings','CD']
+    # set the account list to the accounts you ant to start. I added an extra one. the idea is to have everything passed to the main function
+    account_list=['Savings','CD','Checking']
     # Call the main function.
     main(account_list)
