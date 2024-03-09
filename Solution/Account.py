6d167910c5939f4dcafaf49844fddf346a20ad59
ehxewri <frank.hanan@fjhit.com>
2024-03-09 15:36:42 -0600

    refactored code - rough draft

--- Solution/Account.py
+++ Solution/Account.py
@@ -2,22 +2,33 @@
 
 class Account:
     """Creating an Account class with methods"""
-    def __init__(self, balance, interest_earned):
-        self.balance = balance
-        self.interest_earned = interest_earned
+    def __init__(self,account, balance, interest_rate,length_of_deposit,interest_earned):
+        self.account            = account 
+        self.balance            = balance
+        self.interest_rate      = interest_rate
+        self.length_of_deposit  = length_of_deposit
+        self.interest_earned    = interest_earned
 
     # This method sets the balance of the account.
-    def set_balance(self, balance, interest_earned):
-        """Sets the balance for the for the account"""
-        self.balance = balance + interest_earned
-        return self.balance
+    def get_account_info(self):
+        self.balance            = float(input(f'\nPlease enter your initial {self.account} account balanace : '))
+        self.interest_rate      = float(input(f'Please enter your {self.account} account interest rate : '))
+        self.length_of_deposit  = int(input(f'Please enter your {self.account} account deposit duration in months : '))
 
     # The method sets the interest gained for the account.
-    def set_interest(self, balance,interest_rate,length_of_deposit):
+    def calc_accrual(self):
         """Sets the interest gained for the the account"""
-        self.interest_earned = (balance*(interest_rate/100/12)*length_of_deposit)
-        return self.interest_earned
+        self.interest_earned    = (self.balance*(self.interest_rate/100/12)*self.length_of_deposit)
 
-class Account_Types:
+    def update_balance(self):
+        """Sets the balance for the for the account"""
+        self.balance            = self.balance + self.interest_earned
+
+    def print_interest_accrued(self):
+        print (f'\nYour {self.account} account interest at maturity for {self.length_of_deposit} months is {self.interest_earned}')
+    def print_updated_balance(self):
+        print (f'Your new {self.account} balance will be {self.balance}\n')
+
+class Accounts:
     def __init__(self,accounts):
         self.accounts = accounts
--- Solution/customer_banking.py
+++ Solution/customer_banking.py
@@ -1,28 +1,34 @@
-# Import the account_Accrual and account_input from Account_action
-from Account_actions import account_input, account_accrual
-from Account import Account, Account_Types
+# import Accounts and Account class from Account
+from Account import Accounts, Account
 # import addional packages to support formating
 from rich.console import Console
 console = Console()
 console.clear()
 # Define the main function
-def main():
-    """this funtiongets the account types for savings and cd (uses account type class)
-        for each account 
-            get account_input
-            caculate account accrual and new balance (uses accout class- think of better descriptive name)
-            print account accrual and new balance
+def main(account_list):
+    # instanciate my_account from Accounts class
+    my_accounts=Accounts(account_list)    
+    """ for each account type
+        Initialize the account
+        calculate the intereste accrued
+        calculat the new balance
+        print interest accrued and new balance
     """ 
-    my_accounts=Account_Types(['Savings','CD'])
     for account in my_accounts.accounts:
-        # initialize new account 
+        # instanciate account from account class 
+        my_account=Account(account,0,0,0,0)
         # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-        starting_balance, interest_rate, length_of_deposit = account_input(account)
-        # Call the create_savings_account function and pass the variables from the user.
-        updated_balance, interest_earned = account_accrual(starting_balance, interest_rate, length_of_deposit)
-        print (f'\nYour {account} account interest at maturity for {length_of_deposit} months is {interest_earned}')
-        print (f'\nYour new {account} balance will be {updated_balance}\n')
+        my_account.get_account_info()
+        # Calculate Interest accrued 
+        my_account.calc_accrual()
+        # update the account balance
+        my_account.update_balance()
+        # print (f'\nYour {account} account interest at maturity for {length_of_deposit} months is {interest_earned}')
+        my_account.print_interest_accrued()
+        # print (f'\nYour new {account} balance will be {updated_balance}\n')
+        my_account.print_updated_balance()
 
 if __name__ == "__main__":
+    account_list=['Savings','CD']
     # Call the main function.
-    main()
+    main(account_list)
